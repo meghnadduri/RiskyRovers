a8ab5fb5a19fb0d84609aa400945819411ca36f4
Prisha <66759675+PrishaTHE-PRO@users.noreply.github.com>
2024-07-24 11:08:31 -0500

    Update RoverSpriteChanger.cs

--- Erica/RoverSpriteChanger.cs
+++ Erica/RoverSpriteChanger.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class RoverSpriteChanger : MonoBehaviour
 {
 
-    public Sprite[] roverSprites;
-    public GameObject rover1;
+    public Sprite[] roverSprites; // all the different rover colors 
+    public GameObject rover1; // the rover character 
     // Start is called before the first frame update
     void Start()
     {
-        int selectedSprite = PlayerPrefs.GetInt("selectedCharacter");
-        rover1.GetComponent<SpriteRenderer>().sprite = roverSprites[selectedSprite];
+        int selectedSprite = PlayerPrefs.GetInt("selectedCharacter");  // gets the selectedCharacter variable from the character selection script 
+        rover1.GetComponent<SpriteRenderer>().sprite = roverSprites[selectedSprite]; // sets the sprite to the rover variable shown in character selection 
     }
 
     
