5e38ccdbd91127e55aa3178ca124600be515d78e
Prisha <66759675+PrishaTHE-PRO@users.noreply.github.com>
2024-07-24 11:12:02 -0500

    Update GameManagerScore.cs

--- _Scripts/GameManagerScore.cs
+++ _Scripts/GameManagerScore.cs
@@ -6,16 +6,16 @@ using TMPro;
 public class GameManagerScore : MonoBehaviour
 {
 
-    public TextMeshProUGUI mainText;
+    public TextMeshProUGUI mainText; // score text in the heirarchy
     public int score = 0;
    
 
     void Start() {
-        mainText.text = "Rocks Found: " + score + "/6";
+        mainText.text = "Rocks Found: " + score + "/6"; // starting line when game starts 
     }
     public void UpdateScore(int s) {
         score += s;
-        mainText.text = "Rocks Found: " + score + "/6";
+        mainText.text = "Rocks Found: " + score + "/6"; // sets score to +1
     }
 
 }
