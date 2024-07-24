0cf01043ca44b369018266074e3d6978c4882038
Prisha <66759675+PrishaTHE-PRO@users.noreply.github.com>
2024-07-24 11:07:30 -0500

    Update LoadCharacter.cs

--- Erica/LoadCharacter.cs
+++ Erica/LoadCharacter.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class LoadCharacter : MonoBehaviour
+public class LoadCharacter : MonoBehaviour // NOT RELEVANT SCRIPT - NOT CURRENTLY BEING USED IN GAME 
 {
     //public GameObject [] characterPrefabs;
     public Sprite[] characterPrefabs;
