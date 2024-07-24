4a1a2ffa5069bac7f17fb5d88e83c6f20563caf8
Prisha <66759675+PrishaTHE-PRO@users.noreply.github.com>
2024-07-24 11:10:26 -0500

    Update rockexperiment.cs

--- Erica/rockexperiment.cs
+++ Erica/rockexperiment.cs
@@ -9,16 +9,16 @@ public class rockexperiment : MonoBehaviour
 
 
     void Start() {
-        experimentButton = GameObject.Find("ExperimentImage").GetComponent<ExperimentImage>();
+        experimentButton = GameObject.Find("ExperimentImage").GetComponent<ExperimentImage>(); // finds the experiment image in heirarchy 
     }
     private void OnTriggerEnter2D(Collider2D x) {
 
         
-        experimentButton.ChangeImage(rockNum);
+        experimentButton.ChangeImage(rockNum); // when the rover enters the trigger child from the rock, the experiment image should change 
     }
 
     private void OnTriggerExit2D(Collider2D y) {
-        experimentButton.ResetImage();
+        experimentButton.ResetImage(); // when exiting the trigger it resets image. 
     }
 
     
